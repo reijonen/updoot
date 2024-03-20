1f8ba25dc5e38f34ab61db011366fad838b509da
reijonen <samuli.o.reijonen@gmail.com>
2024-03-20 13:20:51 +0200

    fix chrome should be run in the background

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -11,6 +11,6 @@ npm install
 	--window-position=0,0 \
 	--ignore-certifcate-errors \
 	--ignore-certifcate-errors-spki-list \
-	--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"
+	--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36" &
 
 npm run dev
